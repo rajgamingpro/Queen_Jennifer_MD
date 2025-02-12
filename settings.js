0bb614891a34be1499106e1e124a55ff890b3087
rajgamingpro <rajgamingproyt@gmail.com>
2025-02-12 19:10:24 +0530

    Raj

--- settings.js
+++ settings.js
@@ -13,10 +13,10 @@ const { ownername, botname, footer, ig, region, myweb, packname, author, session
 
 
 //customize settings\\
-global.APIKeys = {'https://zenzapis.xyz': YOUR_API_KEY } //credit goes to sanzy yt, if api key expire, buy it and put it here?\\ https://zenzapis.xyz
-global.owner = ['94761539856'] // Add Your number Eg: ['94761539856', 'YOUR NUMBER']
-global.premium = ['94761539856'] // Add Your number Eg: ['94761539856', 'YOUR NUMBER']
-global.ownernomer = '94761539856' // Add Your number Eg: ['YOUR NUMBER']
+global.APIKeys = {'https://zenzapis.xyz': YOUR_API_KEY Raj } //credit goes to sanzy yt, if api key expire, buy it and put it here?\\ https://zenzapis.xyz
+global.owner = ['919394560158'] // Add Your number Eg: ['94761539856', 'YOUR NUMBER']
+global.premium = ['919394560158'] // Add Your number Eg: ['94761539856', 'YOUR NUMBER']
+global.ownernomer = '919394560158' // Add Your number Eg: ['YOUR NUMBER']
 // we got your name in your whatsapp
 
 
